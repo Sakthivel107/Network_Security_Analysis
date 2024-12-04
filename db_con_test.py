37ba825019432f5a72b772a943a5d2bc70af50da
Sakthivel107 <sakthivel107g@gmail.com>
2024-12-04 09:15:08 +0530

    code updated

--- db_con_test.py
+++ db_con_test.py
@@ -2,10 +2,10 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-url= "mongodb+srv://Sakthivel:8072656893@mlcluster.33fsw.mongodb.net/?retryWrites=true&w=majority&appName=MLcluster"
+uri= "mongodb+srv://Sakthivel:8072656893@mlcluster.33fsw.mongodb.net/?retryWrites=true&w=majority&appName=MLcluster"
 
 # Create a new client and connect to the server
-client = MongoClient(url, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
--- getdata.py
+++ getdata.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 
-
+import pymongo
 from dotenv import load_dotenv
 load_dotenv()
 Mongo_db_url=os.getenv('MONGODB_url')
@@ -11,3 +11,40 @@ import certifi
 ca=certifi.where()
 import numpy as np
 import pandas as pd
+from NetworkSecurity.exception.exception import MyException
+from NetworkSecurity.logger.logger import logging
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise MyException(e,sys)
+    def CovertsIntoJson(self,filepath):
+        try:
+            data=pd.read_csv(filepath)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise MyException(e,sys)
+    def PushingDataIntoMongoDB(self,records,database,collection):
+        try:
+            self.records=records
+            self.database=database
+            self.collection=collection
+
+            self.mongo_client=pymongo.MongoClient(Mongo_db_url)
+            self.database=self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+        except Exception as e:
+            raise MyException(e,sys)
+if __name__=="__main__":
+    FILE_PATH='Network_data\\NetworkData.csv'
+    DATABASE="mlcluster"
+    COLLECTION="NETWORK_SEQURITY"
+    networkobj=NetworkDataExtract()
+    records= networkobj.CovertsIntoJson(FILE_PATH)
+    no_of_records=networkobj.PushingDataIntoMongoDB(records,DATABASE,COLLECTION)
+    print(no_of_records)
